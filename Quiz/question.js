d1bd775c466fed86b49febe1602683d5e0a5fc13
saimaharis <saima.harisahmed@gmail.com>
2019-04-22 10:15:26 +0500

    Done

---
+++ Quiz/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": " What is the meaning of Pakistan?",
+	"option1": "Holy Land",
+	"option2": "Desert",
+	"option3": "Land of five rivers",
+	"option4": "Unity",
+	"answer": "2"
+}, {
+	"question": "What is the capital city of Pakistan?",
+	"option1": "Karachi ",
+	"option2": "Islamabad",
+	"option3": "Lahore",
+	"option4": "Punjab",
+	"answer": "2"
+}, {
+	"question": " Who was the first Governor General of Pakistan?",
+	"option1": "Muhammad Ali Jinnah",
+	"option2": "Liaquat Ali Khan",
+	"option3": "Ayub Khan",
+	"option4": "Sikandar Mirza",
+	"answer": "1"
+}, {
+	"question": "Where is Quaid -e- Azam's Tomb?",
+	"option1": "Islamabad",
+	"option2": "Quetta",
+	"option3": "Lahore",
+	"option4": "Karachi",
+	"answer": "4"
+}, {
+	"question": "Which is the national flower of Pakistan? ?",
+	"option1": "Rose",
+	"option2": "Tulips",
+	"option3": "Jasmine",
+	"option4": "Camomille",
+	"answer": "3"
+}, {
+	"question": "Which is the national language of Pakistan?",
+	"option1": "Hindi",
+	"option2": "Urdu",
+	"option3": "English",
+	"option4": "Bengali",
+	"answer": "2"
+}, {
+	"question": "Which is the national bird of Pakistan?",
+	"option1": "Eagle",
+	"option2": "Crow",
+	"option3": "Chakor",
+	"option4": "Peacock",
+	"answer": "3"
+}, {
+	"question": "How many letters are there in Urdu alphabets:?",
+	"option1": "35",
+	"option2": "36",
+	"option3": "37",
+	"option4": "39",
+	"answer": "3"
+}, {
+	"question": "Who composed the verses of Pakistan national Anthem?",
+	"option1": "Hafeez Jallandri",
+	"option2": "Nasir",
+	"option3": "Allama Iqbal",
+	"option4": "Faiz Ahmed Faiz",
+	"answer": "1"
+}, {
+	"question": "Which is the tallest building in Pakistan?",
+	"option1": "MCB",
+	"option2": "Habib Bank Plaza",
+	"option3": "Bahria Icon Tower ",
+	"option4": "UBL",
+	"answer": "3"
+}];
